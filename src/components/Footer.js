f6a81d69349e0c19adf33d2c902159dfdc7c7b6c
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-01-25 16:21:29 -0500

    removed api fetch for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../styles/Footer.css';
 
 function Footer() {
-
-  /*
-  const [quote, setQuote] = useState({});
-
-  useEffect(() => {
-    const fetchRandomQuote = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8080/api/quotes/random');
-        const data = await response.json();
-        setQuote(data);
-      } catch (error) {
-        console.error('Error fetching quote:', error);
-      }
-    };
-    fetchRandomQuote();
-  }, []);
-  */
-
   return (
     <div className='footer'>
       <div className='footer-line'></div>
