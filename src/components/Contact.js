cff9347f579f366249878285a8d72eb0e34a6c9c
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-03-18 12:17:28 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import { GrLinkedin } from 'react-icons/gr';
 import emailjs from '@emailjs/browser';
 import toast, { Toaster } from 'react-hot-toast';
 import '../styles/Contact.css';
-import { Fade, Zoom } from 'react-awesome-reveal';
+import { Fade } from 'react-awesome-reveal';
 
 function Contact() {
   const formRef = useRef();
